b5205c854bdd52038a745bffa0769c23f8d4583b
ref@LiHaiFei <ref@LiHaiFei>
2025-11-01 19:07:01 +0800

    added 'REPL'

--- app/main.go
+++ app/main.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		panic(err)
-	} else {
-		commandWithoutNewLine := command[:len(command)-1]
-		fmt.Printf("%s: command not found", commandWithoutNewLine)
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic(err)
+		} else {
+			commandWithoutNewLine := command[:len(command)-1]
+			fmt.Printf("%s: command not found\n", commandWithoutNewLine)
+		}
 	}
 }
