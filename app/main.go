b57d50860f3b7757e43116aad6da3fbaf10c61f7
ref@LiHaiFei <ref@LiHaiFei>
2025-11-01 18:38:56 +0800

    now we can just ignore commands.. amazing

--- app/main.go
+++ app/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		panic(err)
+	} else {
+		commandWithoutNewLine := command[:len(command)-1]
+		fmt.Printf("%s: command not found", commandWithoutNewLine)
+	}
 }
