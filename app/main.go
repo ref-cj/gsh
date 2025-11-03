90c2812b3818786082ace8519aa64a6292ff0f66
ref@LiHaiFei <ref@LiHaiFei>
2025-11-03 20:30:54 +0800

    half implemented finding binaries in PATH

--- app/main.go
+++ app/main.go
@@ -8,6 +8,12 @@ import (
 	"strings"
 )
 
+const (
+	generalError      = 1
+	commandUsageError = 2
+	invalidArguments  = 3
+)
+
 type builtin func([]string)
 
 var builtins = make(map[string]builtin)
@@ -21,16 +27,25 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			panic(err)
+			fmt.Println("Could not read input from stdin")
+			os.Exit(commandUsageError)
 		} else {
 			commandFields := strings.Fields(command)
+
+			// is this a builtin?
 			if _builtin, ok := builtins[commandFields[0]]; ok {
 				_builtin(commandFields[1:])
-			} else {
-				fmt.Printf("%v: command not found\n", strings.Join(commandFields, ""))
+				continue
 			}
+
+			// looks like we don't know what this is
+			fmt.Printf("%v: command not found\n", strings.Join(commandFields, ""))
 		}
 	}
+}
+
+func isBinaryInPath(s string) bool {
+	return true
 }
 
 func echo(params []string) {
@@ -40,7 +55,8 @@ func echo(params []string) {
 func exit(code []string) {
 	exitCode, err := strconv.Atoi(code[0])
 	if err != nil {
-		panic(err)
+		fmt.Println("first (and only) argument to exit should be an integer")
+		os.Exit(invalidArguments)
 	} else {
 		os.Exit(exitCode)
 	}
@@ -50,7 +66,17 @@ func toipe(fns []string) {
 	for _, t := range fns {
 		if _, ok := builtins[t]; ok {
 			fmt.Printf("%s is a shell builtin\n", t)
-		} else {
+			continue
+		}
+		if pathValue, exists := os.LookupEnv("PATH"); exists /*&& len(pathValue) > 0*/ {
+			paths := strings.Split(pathValue, string(os.PathListSeparator))
+			fmt.Printf("paths: %v", paths)
+			for _, p := range paths {
+				if isBinaryInPath(p) {
+					fmt.Printf("%s is %s%c%s", t, p, os.PathListSeparator, t)
+					continue
+				}
+			}
 			fmt.Printf("%s: not found\n", t)
 		}
 	}
